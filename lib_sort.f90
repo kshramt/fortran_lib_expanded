7cd58db87588297acec9996a9dedb61c05beeaba
kshramt <thisisdummy@example.com>
2012-12-27 04:16:19 +0900

    cc1123b3a91353107865c6b838315980f5fd6d05

--- lib_sort.f90
+++ lib_sort.f90
@@ -6,6 +6,7 @@
 # 3 "lib_sort.f90" 2
 module lib_sort
   use, intrinsic:: iso_fortran_env, only: INT8, INT16, INT32, INT64, REAL32, REAL64, REAL128
+  use, intrinsic:: iso_fortran_env, only: ERROR_UNIT
   use lib_comparable, only: is_nan
 
   implicit none
@@ -36,12 +37,41 @@ module lib_sort
       module procedure qsortRealDim1KindREAL128
     end interface qsort
 
+    interface div_floor
+      module procedure div_floorIntegerDim1KindINT8
+    end interface div_floor
+
+    interface intermediate_floor_avoid_overflow
+      module procedure intermediate_floor_avoid_overflowIntegerDim1KindINT8
+    end interface intermediate_floor_avoid_overflow
+    interface div_floor
+      module procedure div_floorIntegerDim1KindINT16
+    end interface div_floor
+
+    interface intermediate_floor_avoid_overflow
+      module procedure intermediate_floor_avoid_overflowIntegerDim1KindINT16
+    end interface intermediate_floor_avoid_overflow
+    interface div_floor
+      module procedure div_floorIntegerDim1KindINT32
+    end interface div_floor
+
+    interface intermediate_floor_avoid_overflow
+      module procedure intermediate_floor_avoid_overflowIntegerDim1KindINT32
+    end interface intermediate_floor_avoid_overflow
+    interface div_floor
+      module procedure div_floorIntegerDim1KindINT64
+    end interface div_floor
+
+    interface intermediate_floor_avoid_overflow
+      module procedure intermediate_floor_avoid_overflowIntegerDim1KindINT64
+    end interface intermediate_floor_avoid_overflow
+
 contains
 
     recursive function qsortIntegerDim1KindINT8(a) result(this)
-      Integer(kind = INT8), dimension(:):: this
+      Integer(kind = INT8), dimension(:), allocatable:: this
       Integer(kind = INT8), dimension(:), intent(in):: a
-      Integer(kind = INT8), dimension(:):: aMin, aMax
+      Integer(kind = INT8):: aMin, aMax, pib
 
       if(size(a) <= 1)then
         this = a
@@ -61,25 +91,25 @@ contains
 
     ! this: Floor of m/n.
     function div_floorIntegerDim1KindINT8(m, n) result(this)
-      Integer(kind = INT8), dimension(:):: this
-      Integer(kind = INT8), dimension(:), intent(in):: m, n
+      Integer(kind = INT8):: this
+      Integer(kind = INT8), intent(in):: m, n
 
       this = m/n
-      if(mod(m, n) < 0) this = this - 1
+      if(mod(m, n) < 0) this = this - 1_INT8
     end function div_floorIntegerDim1KindINT8
 
     ! this: Floor of intermediate of m and n without suffering overflow.
     function intermediate_floor_avoid_overflowIntegerDim1KindINT8(m, n) result(this)
-      Integer(kind = INT8), dimension(:):: this
-      Integer(kind = INT8), dimension(:), intent(in):: m, n
+      Integer(kind = INT8):: this
+      Integer(kind = INT8), intent(in):: m, n
 
-      this = div_floor(m, 2) + div_floor(n, 2)
-      if((mod(m, 2) == 1 .and. mod(n, 2) == 1) .or. (mod(m, 2) == -1 .and. mod(n, 2) == -1)) this = this + 1
+      this = div_floor(m, 2_INT8) + div_floor(n, 2_INT8)
+      if((mod(m, 2) == 1 .and. mod(n, 2) == 1) .or. (mod(m, 2) == -1 .and. mod(n, 2) == -1)) this = this + 1_INT8
     end function intermediate_floor_avoid_overflowIntegerDim1KindINT8
     recursive function qsortIntegerDim1KindINT16(a) result(this)
-      Integer(kind = INT16), dimension(:):: this
+      Integer(kind = INT16), dimension(:), allocatable:: this
       Integer(kind = INT16), dimension(:), intent(in):: a
-      Integer(kind = INT16), dimension(:):: aMin, aMax
+      Integer(kind = INT16):: aMin, aMax, pib
 
       if(size(a) <= 1)then
         this = a
@@ -99,25 +129,25 @@ contains
 
     ! this: Floor of m/n.
     function div_floorIntegerDim1KindINT16(m, n) result(this)
-      Integer(kind = INT16), dimension(:):: this
-      Integer(kind = INT16), dimension(:), intent(in):: m, n
+      Integer(kind = INT16):: this
+      Integer(kind = INT16), intent(in):: m, n
 
       this = m/n
-      if(mod(m, n) < 0) this = this - 1
+      if(mod(m, n) < 0) this = this - 1_INT16
     end function div_floorIntegerDim1KindINT16
 
     ! this: Floor of intermediate of m and n without suffering overflow.
     function intermediate_floor_avoid_overflowIntegerDim1KindINT16(m, n) result(this)
-      Integer(kind = INT16), dimension(:):: this
-      Integer(kind = INT16), dimension(:), intent(in):: m, n
+      Integer(kind = INT16):: this
+      Integer(kind = INT16), intent(in):: m, n
 
-      this = div_floor(m, 2) + div_floor(n, 2)
-      if((mod(m, 2) == 1 .and. mod(n, 2) == 1) .or. (mod(m, 2) == -1 .and. mod(n, 2) == -1)) this = this + 1
+      this = div_floor(m, 2_INT16) + div_floor(n, 2_INT16)
+      if((mod(m, 2) == 1 .and. mod(n, 2) == 1) .or. (mod(m, 2) == -1 .and. mod(n, 2) == -1)) this = this + 1_INT16
     end function intermediate_floor_avoid_overflowIntegerDim1KindINT16
     recursive function qsortIntegerDim1KindINT32(a) result(this)
-      Integer(kind = INT32), dimension(:):: this
+      Integer(kind = INT32), dimension(:), allocatable:: this
       Integer(kind = INT32), dimension(:), intent(in):: a
-      Integer(kind = INT32), dimension(:):: aMin, aMax
+      Integer(kind = INT32):: aMin, aMax, pib
 
       if(size(a) <= 1)then
         this = a
@@ -137,25 +167,25 @@ contains
 
     ! this: Floor of m/n.
     function div_floorIntegerDim1KindINT32(m, n) result(this)
-      Integer(kind = INT32), dimension(:):: this
-      Integer(kind = INT32), dimension(:), intent(in):: m, n
+      Integer(kind = INT32):: this
+      Integer(kind = INT32), intent(in):: m, n
 
       this = m/n
-      if(mod(m, n) < 0) this = this - 1
+      if(mod(m, n) < 0) this = this - 1_INT32
     end function div_floorIntegerDim1KindINT32
 
     ! this: Floor of intermediate of m and n without suffering overflow.
     function intermediate_floor_avoid_overflowIntegerDim1KindINT32(m, n) result(this)
-      Integer(kind = INT32), dimension(:):: this
-      Integer(kind = INT32), dimension(:), intent(in):: m, n
+      Integer(kind = INT32):: this
+      Integer(kind = INT32), intent(in):: m, n
 
-      this = div_floor(m, 2) + div_floor(n, 2)
-      if((mod(m, 2) == 1 .and. mod(n, 2) == 1) .or. (mod(m, 2) == -1 .and. mod(n, 2) == -1)) this = this + 1
+      this = div_floor(m, 2_INT32) + div_floor(n, 2_INT32)
+      if((mod(m, 2) == 1 .and. mod(n, 2) == 1) .or. (mod(m, 2) == -1 .and. mod(n, 2) == -1)) this = this + 1_INT32
     end function intermediate_floor_avoid_overflowIntegerDim1KindINT32
     recursive function qsortIntegerDim1KindINT64(a) result(this)
-      Integer(kind = INT64), dimension(:):: this
+      Integer(kind = INT64), dimension(:), allocatable:: this
       Integer(kind = INT64), dimension(:), intent(in):: a
-      Integer(kind = INT64), dimension(:):: aMin, aMax
+      Integer(kind = INT64):: aMin, aMax, pib
 
       if(size(a) <= 1)then
         this = a
@@ -175,30 +205,30 @@ contains
 
     ! this: Floor of m/n.
     function div_floorIntegerDim1KindINT64(m, n) result(this)
-      Integer(kind = INT64), dimension(:):: this
-      Integer(kind = INT64), dimension(:), intent(in):: m, n
+      Integer(kind = INT64):: this
+      Integer(kind = INT64), intent(in):: m, n
 
       this = m/n
-      if(mod(m, n) < 0) this = this - 1
+      if(mod(m, n) < 0) this = this - 1_INT64
     end function div_floorIntegerDim1KindINT64
 
     ! this: Floor of intermediate of m and n without suffering overflow.
     function intermediate_floor_avoid_overflowIntegerDim1KindINT64(m, n) result(this)
-      Integer(kind = INT64), dimension(:):: this
-      Integer(kind = INT64), dimension(:), intent(in):: m, n
+      Integer(kind = INT64):: this
+      Integer(kind = INT64), intent(in):: m, n
 
-      this = div_floor(m, 2) + div_floor(n, 2)
-      if((mod(m, 2) == 1 .and. mod(n, 2) == 1) .or. (mod(m, 2) == -1 .and. mod(n, 2) == -1)) this = this + 1
+      this = div_floor(m, 2_INT64) + div_floor(n, 2_INT64)
+      if((mod(m, 2) == 1 .and. mod(n, 2) == 1) .or. (mod(m, 2) == -1 .and. mod(n, 2) == -1)) this = this + 1_INT64
     end function intermediate_floor_avoid_overflowIntegerDim1KindINT64
 
     recursive function qsortRealDim1KindREAL32(a) result(this)
       Real(kind = REAL32), dimension(:), intent(in):: a
-      Real(kind = REAL32), dimension(:):: this
+      Real(kind = REAL32), dimension(:), allocatable:: this
 
-      Real(kind = REAL32), dimension(:):: aMin, aMax, pib
+      Real(kind = REAL32):: aMin, aMax, pib
       Integer:: maxlocA(1:1), minlocA(1:1)
 
-      if(any(is_nan(a)))then; write(ERROR_UNIT, *) "RAISE: ", "lib_sort.f90", " ", 197,"any(is_nan(a))", " ", a; stop 1; end if
+      if(any(is_nan(a)))then; write(ERROR_UNIT, *) "RAISE: ", "lib_sort.f90", " ", 227,"any(is_nan(a))", " ", a; stop 1; end if
 
       if(size(a) <= 1)then ! Anchor case.
         this = a
@@ -232,12 +262,12 @@ contains
     end function qsortRealDim1KindREAL32
     recursive function qsortRealDim1KindREAL64(a) result(this)
       Real(kind = REAL64), dimension(:), intent(in):: a
-      Real(kind = REAL64), dimension(:):: this
+      Real(kind = REAL64), dimension(:), allocatable:: this
 
-      Real(kind = REAL64), dimension(:):: aMin, aMax, pib
+      Real(kind = REAL64):: aMin, aMax, pib
       Integer:: maxlocA(1:1), minlocA(1:1)
 
-      if(any(is_nan(a)))then; write(ERROR_UNIT, *) "RAISE: ", "lib_sort.f90", " ", 236,"any(is_nan(a))", " ", a; stop 1; end if
+      if(any(is_nan(a)))then; write(ERROR_UNIT, *) "RAISE: ", "lib_sort.f90", " ", 266,"any(is_nan(a))", " ", a; stop 1; end if
 
       if(size(a) <= 1)then ! Anchor case.
         this = a
@@ -271,12 +301,12 @@ contains
     end function qsortRealDim1KindREAL64
     recursive function qsortRealDim1KindREAL128(a) result(this)
       Real(kind = REAL128), dimension(:), intent(in):: a
-      Real(kind = REAL128), dimension(:):: this
+      Real(kind = REAL128), dimension(:), allocatable:: this
 
-      Real(kind = REAL128), dimension(:):: aMin, aMax, pib
+      Real(kind = REAL128):: aMin, aMax, pib
       Integer:: maxlocA(1:1), minlocA(1:1)
 
-      if(any(is_nan(a)))then; write(ERROR_UNIT, *) "RAISE: ", "lib_sort.f90", " ", 275,"any(is_nan(a))", " ", a; stop 1; end if
+      if(any(is_nan(a)))then; write(ERROR_UNIT, *) "RAISE: ", "lib_sort.f90", " ", 305,"any(is_nan(a))", " ", a; stop 1; end if
 
       if(size(a) <= 1)then ! Anchor case.
         this = a
